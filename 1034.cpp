517ad5f2fb15c5a53c4adaaa6c8bfd7f393d137c
tiezhu-lee <lk0219@foxmail.com>
2016-02-16 16:47:52 +0800

    1034 done

--- 1034.cpp
+++ 1034.cpp
@@ -5,26 +5,31 @@
 
 using namespace std;
 
-string trans(int x, int y)
+long long Gcd(long long a, long long b)
+{
+	long long c;
+	while(b!=0)
+	{c = a%b; a = b; b = c ; }
+	return a;
+}
+
+string trans(long long x, long long y)
 {
 	if(y<0 ) {
 		x = -x;
 		y = -y;
 	}
 	stringstream res ;
-	int inte = x / y;
+	long long  inte = x / y;
 	if(x == 0) res << '0';
 	else {
-		int rmd = abs(x%y);
+		long long rmd = abs(x%y);
 		if(x < 0) res << '(';
 		if(inte != 0) res << inte ;
 		if(rmd != 0) {
 			if(inte != 0) res << ' ';
 			else if(x < 0) res << '-';
-			int gcd = 1;
-			for(int i = 2; i <= rmd; i++)
-				if(rmd % i == 0 && y % i == 0) 
-					gcd = i;
+			long long gcd = Gcd(rmd, y);
 			rmd /= gcd;
 			y /= gcd;
 			res << rmd << '/' << y;
@@ -36,24 +41,32 @@ string trans(int x, int y)
 	return tmp;
 }
 
+
 int main()
 {
-	int a1, b1, a2, b2;
+	long long  a1, b1, a2, b2;
 	string op1, op2, res;
-	scanf("%d/%d%d/%d", &a1, &b1, &a2, &b2);
-	//printf("%d %d %d %d\n", a1, b1, a2, b2);
-	int r1, r2;
-
+	scanf("%lld/%lld%lld/%lld", &a1, &b1, &a2, &b2);
+	long long r1, r2;
+	long long gcd = Gcd(a1, b1);
+	a1 /= gcd;
+	b1 /= gcd;
+	gcd = Gcd(a2, b2);
+	a2 /= gcd;
+	b2 /= gcd;
+	
 	/****** plus ***********/
-	r1 = a1 * b2 + a2 * b1;
-	r2 = b1 * b2;
+	gcd = Gcd(b1, b2);
+	r1 = a1 * (b2 / gcd) + a2 * (b1 / gcd);
+	r2 = b1 * (b2 / gcd);
 	op1 = trans(a1, b1);
 	op2 = trans(a2, b2);
 	res = trans(r1, r2);
 	cout << op1 << " + " << op2 << " = " << res << endl;
 
 	/***** minus *********/
-	r1 = a1 * b2 - a2 * b1;
+	r1 = a1 * (b2 / gcd) - a2 * (b1 / gcd);
+
 	res = trans(r1, r2);
 	cout << op1 << " - " << op2 << " = " << res << endl;
 
