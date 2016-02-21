0f484cc30bc4f823344457f6499b0d3ff436c7b9
tiezhu-lee <lk0219@foxmail.com>
2016-02-21 18:50:08 +0800

    1040

---
+++ 1040.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	char c;
+	int idx = 0;
+	int cnt_p = 0, cnt_PA = 0;
+	long long  res = 0;
+	while((c = getchar()) != 'P') ;
+
+	while(c != '\n')
+	{
+		switch(c) {
+		case 'P': cnt_p ++; break;
+		case 'A': cnt_PA += cnt_p;
+				  break;
+		case 'T': 
+				  res += cnt_PA;
+				  break;
+		}
+		idx++;
+		c = getchar();
+	}
+	res %= 1000000007;
+	cout << res <<endl;
+}
